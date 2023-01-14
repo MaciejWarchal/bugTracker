062191582dffd58bec2d24c970fdd2bad73aad88
xyzt <warchal.maciej@gmail.com>
2023-01-14 12:03:12 +0100

    DB connection check

--- src/main/java/com/example/bugtracker/models/Project/Project.java
+++ src/main/java/com/example/bugtracker/models/Project/Project.java
@@ -1,5 +1,24 @@
 package com.example.bugtracker.models.Project;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String code;
+    private String description;
+    private boolean enabled=true;
+
 
 }
--- src/main/java/com/example/bugtracker/models/Project/ProjectDto.java
+++ src/main/java/com/example/bugtracker/models/Project/ProjectDto.java
@@ -2,5 +2,9 @@ package com.example.bugtracker.models.Project;
 
 public class ProjectDto {
 
+private String name;
+private String code;
+private String description;
+private boolean enabled=true;
 
 }
